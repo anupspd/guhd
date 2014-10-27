b9da20072e4b8bc584d0c1e2c9d74911229eccfa
Mandrekar <mandrekar.aditya@yahoo.com>
2014-10-27 11:13:04 +0530

    27-10-2014 11:12 am commit

--- db/schema.rb
+++ db/schema.rb
@@ -20,25 +20,4 @@ ActiveRecord::Schema.define(version: 20141026095716) do
     t.datetime "updated_at"
   end
 
-  create_table "names", force: true do |t|
-    t.string   "email"
-    t.string   "phone"
-    t.string   "address"
-    t.string   "dept"
-    t.string   "designation"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "phone"
-    t.string   "address"
-    t.string   "department"
-    t.string   "designation"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
 end
