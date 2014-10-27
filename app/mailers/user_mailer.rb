6b7ac009de9d2fdc8b0d642db66209dac4dc2cf3
Mandrekar <mandrekar.aditya@yahoo.com>
2014-10-27 12:24:43 +0530

    all

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: 'universitydemo@presentsoft.mailgun.org'
+ 
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Your GU Help desk credentials')
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -20,4 +20,15 @@ ActiveRecord::Schema.define(version: 20141026095716) do
     t.datetime "updated_at"
   end
 
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "department"
+    t.string   "designation"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
